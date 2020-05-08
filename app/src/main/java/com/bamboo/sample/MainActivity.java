8113165e93eb8bb06174754f3c8a72d96f92b79f
sundy <core_it@163.com>
2020-05-08 13:31:10 +0800

    test

--- app/src/main/java/com/bamboo/sample/MainActivity.java
+++ app/src/main/java/com/bamboo/sample/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends BaseActivity {
 //        initWindowManager();
 //        createFloatView("test");
         requestNotification();
+        //test
     }
 
     private void init() {
